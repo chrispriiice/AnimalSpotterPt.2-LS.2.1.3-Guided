1409a7621f5bb17f4b1052c1b5a152076069f758
Chris Price <christopher.r.price@gmail.com>
2020-03-18 21:45:13 -0400

    Initial commit of Steve's code from day 1/first half of the project.

---
+++ AnimalSpotter/View Controllers/LoginViewController.swift
@@ -0,0 +1,91 @@
+//
+//  LoginViewController.swift
+//  AnimalSpotter
+//
+//  Created by Ben Gohlke on 4/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+enum LoginType {
+    case signUp
+    case signIn
+}
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet private weak var signInButton: UIButton!
+    
+    var apiController: APIController?
+    var loginType = LoginType.signUp
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+            signInButton.tintColor = .white
+            signInButton.layer.cornerRadius = 8.0
+    }
+    
+    // MARK: - Action Handlers
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        guard let apiController = apiController else { return }
+        
+        guard let username = usernameTextField.text,
+            username.isEmpty == false,
+            let password = passwordTextField.text,
+            password.isEmpty == false else {
+                return
+        }
+        
+        let user = User(username: username, password: password)
+        
+        switch loginType {
+        case .signUp:
+            apiController.signUp(with: user) { (error) in
+                guard error == nil else {
+                    print("Error signing up: \(error!)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    
+                    self.present(alertController, animated: true) {
+                        self.loginType = .signIn
+                        self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                        self.signInButton.setTitle("Sign In", for: .normal)
+                    }
+                }
+            }
+        case .signIn:
+            apiController.signIn(with: user) { (error) in
+                guard error == nil else {
+                    print("Error loggin in: \(error!)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInButton.setTitle("Sign In", for: .normal)
+        }
+    }
+}
